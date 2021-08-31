cc453b7e3e21c98438bbcc56747dffdf0de92fc3
Alison Cribb <86627266+atcribb@users.noreply.github.com>
2021-08-31 10:43:13 -0700

    Update Sim7_StrongBioturbation.R

--- Simulations/Sim7_StrongBioturbation.R
+++ Simulations/Sim7_StrongBioturbation.R
@@ -1,4 +1,4 @@
-#Authors: Sebastiaan J. van de Velde, Alison T. Cribb
+#Authors: Alison Cribb, Sebastiaan van de Velde
 #Simulation: Strong biomixing + strong bioirrigation
 #Use biogeochemical model function from Model_Function.R script 
 
