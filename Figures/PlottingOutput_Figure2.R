8ae579640039c8b117e5e5f66e6e9148275e765c
Alison Cribb <86627266+atcribb@users.noreply.github.com>
2022-01-24 15:56:59 -0800

    Update PlottingOutput_Figure2.R

--- Figures/PlottingOutput_Figure2.R
+++ Figures/PlottingOutput_Figure2.R
@@ -9,8 +9,13 @@ library(tayloRswift)
 
 #===== PROCESS AND PLOT OUTPUT =====#
 #Load data: 
-load('BiodiffusionSensitivityTestOutput.RData')
-View(results_df)
+load('BiodiffusionSensitivityTestOutput_low.RData')
+low_results <- results_df
+load('BiodiffusionSensitivityTestOutput_med.RData')
+med_results <- results_df
+load('BiodiffusionSensitivityTestOutput_high.RData')
+high_results <- results_df
+
 
 bioturbation_data <- read_csv("~/Desktop/Manucripts/OPD Controls/Submission_ScienceAdvances/Dataset/Db_MLD_foranlysis.csv")
 
@@ -28,14 +33,39 @@ Dbs_globalavs$Db  <- bioturbation_data[7:8,]$Db_final
 
 Dbpalette <- swift_palettes$taylor1989
 
-Db_SA_out <- ggplot() + 
-  #geom_vline(data=Dbs_globalavs, aes(xintercept=Db), color=Dbpalette[3:4], linetype='longdash', size=1.2) +
+Db_SA_out_low <- ggplot() + 
   geom_vline(xintercept=c(0.1,0.98), color=Dbpalette[3:4], linetype='longdash', size=1.2) +
-  geom_path(data=results_df, aes(x=Db, y=OPD, color=as.factor(O2_bw))) + #plot points 
-  geom_point(data=results_df, aes(x=Db, y=OPD, color=as.factor(O2_bw))) + #plot path 
+  geom_path(data=low_results, aes(x=Db, y=OPD, color=as.factor(O2_bw))) + #plot points 
+  geom_point(data=low_results, aes(x=Db, y=OPD, color=as.factor(O2_bw))) + #plot path 
   scale_color_taylor(palette='taylorRed') +
   xlab(expression(paste(D[B]~(cm^{2}~yr^{-1})))) +
   ylab('Oxygen Penetration Depth (cm)') +
   scale_y_reverse(limits=c(15,0)) +
+  ggtitle(expression(paste('Organic matter flux = 150', ~mu, mol~cm^{-2}, yr^{-1}))) +
   theme_classic()     
-Db_SA_out  
+
+
+Db_SA_out_med <- ggplot() + 
+  geom_vline(xintercept=c(0.1,0.98), color=Dbpalette[3:4], linetype='longdash', size=1.2) +
+  geom_path(data=med_results, aes(x=Db, y=OPD, color=as.factor(O2_bw))) + #plot points 
+  geom_point(data=med_results, aes(x=Db, y=OPD, color=as.factor(O2_bw))) + #plot path 
+  scale_color_taylor(palette='taylorRed') +
+  xlab(expression(paste(D[B]~(cm^{2}~yr^{-1})))) +
+  ylab('Oxygen Penetration Depth (cm)') +
+  scale_y_reverse(limits=c(15,0)) +
+  ggtitle(expression(paste('Organic matter flux = 400', ~mu, mol~cm^{-2}, yr^{-1}))) +
+  theme_classic()     
+
+Db_SA_out_high <- ggplot() + 
+  geom_vline(xintercept=c(0.1,0.98), color=Dbpalette[3:4], linetype='longdash', size=1.2) +
+  geom_path(data=high_results, aes(x=Db, y=OPD, color=as.factor(O2_bw))) + #plot points 
+  geom_point(data=high_results, aes(x=Db, y=OPD, color=as.factor(O2_bw))) + #plot path 
+  scale_color_taylor(palette='taylorRed') +
+  xlab(expression(paste(D[B]~(cm^{2}~yr^{-1})))) +
+  ylab('Oxygen Penetration Depth (cm)') +
+  scale_y_reverse(limits=c(15,0)) +
+  ggtitle(expression(paste('Organic matter flux = 700', ~mu, mol~cm^{-2}, yr^{-1}))) +
+  theme_classic()     
+
+
+ggarrange(Db_SA_out_low, Db_SA_out_med, Db_SA_out_high, ncol=1)
